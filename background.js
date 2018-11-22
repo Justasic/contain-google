ff03f161c52dd9a467ed444327bded0c369e41ef
Justin Crawford <Justasic@Gmail.com>
2018-11-21 21:14:37 -0800

    Add soundcloud.com

--- background.js
+++ background.js
@@ -33,10 +33,15 @@ const DEVELOPER_DOMAINS = [
   "madewithcode.com", "design.google", "gallery.io", "domains.google", "material.io", "android.com", "chromium.org", "cobrasearch.com", "chromecast.com", "chrome.com", "chromebook.com", "madewithcode.com", "whatbrowser.org", "withgoogle.com",
 ];
 
+// While not a google service, I do use google to login to soundcloud.
+const OTHER_DOMAINS = [
+  "soundcloud.com"
+];
+
 
 GOOGLE_DOMAINS = GOOGLE_DOMAINS.concat(GOOGLE_INTL_DOMAINS)
   .concat(GOOGLE_SERVICES).concat(YOUTUBE_DOMAINS).concat(BLOGSPOT_DOMAINS).concat(ALPHABET_DOMAINS)
-  .concat(DEVELOPER_DOMAINS).concat(AD_DOMAINS);
+  .concat(DEVELOPER_DOMAINS).concat(AD_DOMAINS).concat(OTHER_DOMAINS);
 
 
 
